75c3fe60acafbacf349be2e9af68b517bc980613
Rokcas <rokas0090@gmail.com>
2019-02-19 12:01:20 +0200

    Build fix for deploy

--- build_scripts/Build.ps1
+++ build_scripts/Build.ps1
@@ -1,3 +1,4 @@
 $ErrorActionPreference = "Stop"
+
 Invoke-Expression "dotnet build"
-Invoke-Expression "dotnet publish BarcodeScanner -o artifacts"
+Invoke-Expression "dotnet publish BarcodeScanner -o artifacts -r 'win7-x64' --self-contained"
