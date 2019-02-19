72349a740418fe804f797647d26db5b35fe50c04
Rokcas <rokas0090@gmail.com>
2019-02-19 12:15:04 +0200

    Beno fixas

--- build_scripts/Deploy.ps1
+++ build_scripts/Deploy.ps1
@@ -1,13 +1,14 @@
 $ErrorActionPreference = "Stop"
 
-$serviceFolder = "C:\Services\Roko"
-$serviceName = "RokoBarcodeScanner"
-$artifactsFolder = "modestas"
+$serviceFolder = "C:\Services\Beno"
+$serviceName = "BenoBarcodeScanner"
+$artifactsFolder = "package"
 
 Remove-Item $serviceFolder -Recurse -ErrorAction Ignore
 Copy-Item $artifactsFolder -Destination $serviceFolder -Recurse
 
-Remove-Service -Name $serviceName -ErrorAction Ignore
+Stop-Service -Name $serviceName -ErrorAction Ignore
+Invoke-Expression "sc delete $serviceName"
 New-Service -Name $serviceName -BinaryPathName "$serviceFolder\BarcodeScanner.exe"
 
 Start-Service -Name $serviceName
