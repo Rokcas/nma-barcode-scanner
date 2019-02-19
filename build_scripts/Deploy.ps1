ab352f124738eeda4e455307429ceeb97b6d3305
Rokcas <rokas0090@gmail.com>
2019-02-19 11:39:28 +0200

    Deploy fix

--- build_scripts/Deploy.ps1
+++ build_scripts/Deploy.ps1
@@ -1,8 +1,12 @@
 $ErrorActionPreference = "Stop"
 
-$serviceFolder = "C:\Services\Reno"
-$serviceName = "RenoBarcodeScanner"
+$serviceFolder = "C:\Services\Roko"
+$serviceName = "RokoBarcodeScanner"
 $artifactsFolder = "modestas\*"
+
+If((Test-Path $artifactsFolder) -ne $true) {
+    New-Item $artifactsFolder      
+}
 
 Copy-Item $artifactsFolder $serviceFolder
 New-Service -Name $serviceName -BinaryPathName "$serviceFolder\BarcodeScanner.dll"
